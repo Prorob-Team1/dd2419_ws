121105adf717643f2751d20cd75d6b989c440573
Daniel <danieldejonge@t-online.de>
2026-02-17 16:50:13 +0100

    add: tf map2odom from start pose

--- src/napping/napping/mapping.py
+++ src/napping/napping/mapping.py
@@ -9,7 +9,9 @@ from rclpy.executors import MultiThreadedExecutor, SingleThreadedExecutor
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup, ReentrantCallbackGroup
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
-
+from tf2_ros import TransformBroadcaster, StaticTransformBroadcaster
+from geometry_msgs.msg import TransformStamped
+from tf_transformations import quaternion_from_euler, euler_from_quaternion
 
 import numpy as np
 import csv
@@ -44,6 +46,7 @@ class Mapper(Node):
 
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
+        self.tf_broadcaster = StaticTransformBroadcaster(self)
         self.workspace_pub = self.create_publisher(
             Marker,
             "/geofence",
@@ -51,8 +54,10 @@ class Mapper(Node):
         )
         # if you miss the initial publish, this will republish occasioanlly
         self.marker_timer = self.create_timer(
-            10.0, self.publish_workspace_perimeter_marker
+            5.0, self.publish_workspace_perimeter_marker
         )
+        # Periodic timer for simulating dynamic transform publishing
+        # self.tf_timer = self.create_timer(0.05, self.publish_tf_map2odom)
 
         self.map_dir = Path("./maps")
         self.workspace_file = self.map_dir / "workspace_1.csv"
@@ -73,13 +78,14 @@ class Mapper(Node):
             f"Parsed workspace with {len(self.workspace)} points, {len(self.given_boxes)} boxes, {len(self.given_objects)} objects, start pose {self.start_pose}"
         )
         self.publish_workspace_perimeter_marker()
+        self.publish_tf_map2odom()
 
     def parse_map_file(self, file: Path):
         objects = []
         with open(file, "r", encoding="utf-8-sig") as f:
             reader = csv.DictReader(f)
             # Normalize fieldnames by stripping whitespace
-            reader.fieldnames = [field.strip() for field in reader.fieldnames]
+            reader.fieldnames = [field.strip() for field in reader.fieldnames]  # type: ignore
             for row in reader:
                 obj_type = row["Type"].strip()
                 x = float(row["x"].strip()) / 100.0
@@ -135,6 +141,22 @@ class Mapper(Node):
         )  # Close the polygon by adding the first point at the end
         marker.points = points
         self.workspace_pub.publish(marker)
+
+    def publish_tf_map2odom(self):
+        # TODO: will be dynamic with proper localization
+        t = TransformStamped()
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = "map"
+        t.child_frame_id = "odom"
+        t.transform.translation.x = self.start_pose.x
+        t.transform.translation.y = self.start_pose.y
+        t.transform.translation.z = 0.0
+        q = quaternion_from_euler(0, 0, self.start_pose.angle)
+        t.transform.rotation.x = q[0]
+        t.transform.rotation.y = q[1]
+        t.transform.rotation.z = q[2]
+        t.transform.rotation.w = q[3]
+        self.tf_broadcaster.sendTransform(t)
 
 
 def main():
