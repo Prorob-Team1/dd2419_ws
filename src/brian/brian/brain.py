918ea25b60d7c9eee124837d94809104b53e89e0
Daniel <danieldejonge@t-online.de>
2026-02-23 11:15:23 +0100

    add: action dummy server for behaviour tree testing

--- src/brian/brian/brain.py
+++ src/brian/brian/brain.py
@@ -21,11 +21,14 @@ from robp_interfaces.msg import Encoders, DutyCycles
 from robp_interfaces.action import Navigation
 from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseStamped
-from rclpy.action.server import ActionServer, ServerGoalHandle
+from rclpy.action.client import ActionClient
 from rclpy.executors import MultiThreadedExecutor
 from py_trees.composites import Sequence, Selector, Parallel
 from py_trees.behaviour import Behaviour
 from py_trees.common import Status
+from action_msgs.msg import GoalStatus
+
+from robp_interfaces.action import DummyAction
 
 
 class Brain(Node):
@@ -34,8 +37,50 @@ class Brain(Node):
         super().__init__("brain")
         self.tick_period = 0.1
 
-        self.root = py_trees.composites.Sequence("Root", memory=False)
-        self.create_timer(self.tick_period, self.root.tick_once)
+        # self.root = py_trees.composites.Sequence("Root", memory=False)
+        # self.create_timer(self.tick_period, self.root.tick_once)
+
+        self._dummy_action_client = ActionClient(self, DummyAction, "dummy")
+        while not self._dummy_action_client.wait_for_server(timeout_sec=5.0):
+            self.get_logger().info("Waiting for dummy action server...")
+
+        self._dummy_action_client.wait_for_server()
+        goal = DummyAction.Goal(succeed=True)
+        send_goal_future = self._dummy_action_client.send_goal_async(
+            goal, feedback_callback=self.dummy_feedback_callback
+        )
+        send_goal_future.add_done_callback(self.dummy_goal_response_callback)
+
+    def dummy_feedback_callback(self, feedback_msg):
+        self.get_logger().info(f"Feedback: {feedback_msg.feedback.status}")
+
+    def dummy_goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info("Goal rejected")
+            return
+        self.get_logger().info("Goal accepted")
+        result_future = goal_handle.get_result_async()
+        result_future.add_done_callback(self.dummy_result_callback)
+
+    def dummy_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info(f"Result received: success={result}")
+        self.get_logger().info(f"Result: success={result.success}")
+
+        try:
+            response = future.result()
+            result = response.result
+
+            if response.status == GoalStatus.STATUS_SUCCEEDED:
+                self.get_logger().info(f"Action goal succeeded! {result}")
+            else:
+                self.get_logger().error(
+                    f"Action goal failed with status: {response.status}"
+                )
+
+        except Exception as e:
+            self.get_logger().error(f"Action goal failed: {e}")
 
 
 class GrabCubeB(Behaviour):
