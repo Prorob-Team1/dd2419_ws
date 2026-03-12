3b5b0ed1baa11a8ee36062d8f2539a050e81c761
oskarwsson <oskar.wilhelmsson00@gmail.com>
2026-03-12 17:57:41 +0100

    add:  2d ndt, looked worse than pure icp add: ang vel check

--- src/localization/src/localization.cpp
+++ src/localization/src/localization.cpp
@@ -2,6 +2,7 @@
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/buffer.h>
+#include <tf2/utils.hpp>
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
