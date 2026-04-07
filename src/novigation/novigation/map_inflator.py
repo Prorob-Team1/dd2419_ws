3b5fc19961bad8b94088fc22e693e6c6a1f697e4
Dariush Poursasan <dariush.poursasan@gmail.com>
2026-04-07 14:35:55 +0200

    Increase inflation

--- src/novigation/novigation/map_inflator.py
+++ src/novigation/novigation/map_inflator.py
@@ -17,7 +17,7 @@ class MapInflator(Node):
         super().__init__('map_inflator')
 
       
-        self.inflation_radius_m = 0.15
+        self.inflation_radius_m = 0.25
         self.cost_inflation_radius_m = 0.4
 
         self.base_grid = None    
